197861d853094f87c3946bc829c510645dbf38a7
Elena Kushner <85772107+elkush@users.noreply.github.com>
2022-07-21 16:52:48 +1000

    first commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+export default function Navbar () {
+    return (
+      
+         <nav>
+                <img alt="reactlogo" src="./images/reactjs-icon.png" className="nav-icon" ></img>
+                <h1 className="nav-text">ReactFacts</h1>
+                <h2 className="nav-title">React Course - Project 1</h2>
+        </nav>
+       
+    )
+}
